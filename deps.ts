e12708fa2b29067964616b759d45508fad18c122
Gonçalo Cabrita <_@gmcabrita.com>
2022-12-20 16:04:36 +0000

    feat: remove base support for syntax highlighting

--- deps.ts
+++ deps.ts
@@ -31,40 +31,6 @@ export { default as callsites } from "https://raw.githubusercontent.com/kt3k/cal
 export { Feed, type Item as FeedItem } from "https://esm.sh/feed@4.2.2";
 export { default as removeMarkdown } from "https://esm.sh/remove-markdown@0.5.0";
 
-// Add syntax highlighting support
-import "https://esm.sh/prismjs@1.28.0/components/prism-bash?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-c?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-css?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-cue?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-diff?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-docker?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-elixir?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-erlang?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-go?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-go-module?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-markup?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-http?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-javascript?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-json?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-makefile?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-nginx?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-nix?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-plantuml?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-powershell?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-promql?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-python?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-regex?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-ruby?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-rust?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-sass?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-scss?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-shell-session?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-sql?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-toml?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-typescript?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-yaml?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-zig?no-check";
-
 export { UnoCSS };
 export type UnoConfig = typeof UnoCSS extends (
   arg: infer P | undefined,
